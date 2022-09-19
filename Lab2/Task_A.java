db4adc95a2bacb490bd5daea6442f3f4dc751c43
Kiril Polozhenets <lolak_228@ukr.net>
2022-09-19 20:23:01 +0300

    redone lab1

--- Lab2/Task_A.java
+++ Lab2/Task_A.java
@@ -2,43 +2,70 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Task_A {
-    public static final int forest_size = 25000;
     public static final int numberOfBees = 6;
-    public static Control manager = new Control();
-    public static int[][] forest = new int[forest_size][forest_size];
+
 
     public static void main(String[] args) {
 
-       Pooh pooh = new Pooh();
-        forest[pooh.row][pooh.col] = 1;
+        Forest forest =new Forest();
 
         Bee[] bee_hive = new Bee[numberOfBees];
 
         for (int i = 0; i < numberOfBees; i++) {
-            bee_hive[i] = new Bee("Bee" + String.valueOf(i));
+            bee_hive[i] = new Bee("Bee" + String.valueOf(i),forest);
             Thread thread = new Thread(bee_hive[i]);
             thread.start();
         }
     }
 }
 
-class Pooh {
-    public int row;
-    public int col;
+class Forest{
+    public static final int SIZE=300;
+    private int[][] forest = new int[SIZE][SIZE];
+    private  AtomicInteger counter=new AtomicInteger(0);
+    private AtomicBoolean isFound=new AtomicBoolean(false);
 
-    public Pooh(){
-        this.row = (int) (Math.random() * Task_A.forest_size);
-        this.col =(int)  (Math.random() * Task_A.forest_size);
+
+    public boolean isForestEmpty(){
+        return counter.get()<0;
     }
+
+    public int getCounter(){
+        return counter.get();
+    }
+
+    public boolean iSpoofFind(){
+        return isFound.get();
+    }
+    public void setPoohFind(boolean is_f){
+        this.isFound.set(is_f);
+    }
+     public int[] getRow(){
+        if(!isForestEmpty() && counter.get()<SIZE-1) {
+            return forest[counter.getAndIncrement()];
+        }
+        else{
+            return forest[SIZE-1];
+        }
+    }
+
+    public Forest(){
+        forest[(int) (Math.random()*SIZE)][(int) (Math.random()*SIZE)] = 1;
+    }
+
 }
+
+
 
 class Bee implements Runnable {
     private String beeName;
-    private int row = 0;
 
-    Bee(String name) {
+    private Forest forest;
+    Bee(String name,Forest forest) {
         this.beeName = name;
+        this.forest=forest;
     }
+
 
     public String getBeeName() {
         return beeName;
@@ -50,17 +77,12 @@ class Bee implements Runnable {
 
     @Override
     public void run() {
-        while (!Task_A.manager.IsFound.get()) {
-            synchronized (Task_A.manager) {
-                row = Task_A.manager.counter.getAndIncrement();
-            }
-            System.out.println(beeName + " - " + row);
-            for (int i = 0; i < Task_A.forest_size; i++) {
-                if (Task_A.forest[row][i] == 1 && !Task_A.manager.IsFound.get() && Task_A.manager.counter.get()<Task_A.forest_size) {
-                    Task_A.manager.IsFound.set(true);
-
-                    System.out.println(beeName + " found on " + row);
-
+        while (!forest.isForestEmpty() && !forest.iSpoofFind()) {
+            for (int i = 0; i < forest.SIZE; i++) {
+                System.out.println(beeName + " - " + forest.getCounter());
+                if (forest.getRow()[i] == 1 && !forest.isForestEmpty() ) {
+                    forest.setPoohFind(true);
+                    System.out.println(beeName + " found on " + forest.getCounter());
                     break;
                 }
             }
@@ -69,14 +91,4 @@ class Bee implements Runnable {
 }
 
 
-class Control {
-    public  AtomicInteger counter=new AtomicInteger();
-    public  AtomicBoolean IsFound=new AtomicBoolean();
 
-    Control(){
-        this.counter.set(0);
-        this.IsFound.set(false);
-    }
-
-
-}
